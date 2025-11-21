c637e1ce3064223d14eb474750b2dacd3851d299
suraj-shelke <shelkesuraj205@gmail.com>
2025-11-21 23:44:38 +0530

    order service

---
+++ src/main/java/com/sp/eshopping_order_service/EshoppingOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/eshopping_order_service/EshoppingOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
