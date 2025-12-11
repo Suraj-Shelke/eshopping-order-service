64bba45c0a98f225312772c4fad91bf4cee03b1d
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-11 17:13:14 +0530

    req model created

---
+++ src/main/java/com/sp/eshopping_order_service/model/ProductDetails.java
@@ -0,0 +1,18 @@
+package com.sp.eshopping_order_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public  class ProductDetails {
+
+    private String productName;
+    private long productId;
+    private long quantity;
+    private long price;
+}
