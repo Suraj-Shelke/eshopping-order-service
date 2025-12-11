97466dbf1c799f65a170d186316651d9292bf60d
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-11 16:53:19 +0530

    payload dto

---
+++ src/main/java/com/sp/eshopping_order_service/payload/request/PaymentRequest.java
@@ -0,0 +1,19 @@
+package com.sp.eshopping_order_service.payload.request;
+
+import com.sp.eshopping_order_service.utils.PaymentMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentRequest {
+    private long orderId;
+    private long amount;
+    private String referenceNumber;
+    private PaymentMode paymentMode;
+
+}
