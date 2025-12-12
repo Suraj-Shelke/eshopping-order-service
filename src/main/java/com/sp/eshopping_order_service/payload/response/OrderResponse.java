1d9d34daa9987f5fb4d055db28e97b2f513deb9f
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-12 19:15:36 +0530

    ord response

---
+++ src/main/java/com/sp/eshopping_order_service/payload/response/OrderResponse.java
@@ -0,0 +1,23 @@
+package com.sp.eshopping_order_service.payload.response;
+
+import com.sp.eshopping_order_service.model.PaymentDetails;
+import com.sp.eshopping_order_service.model.ProductDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResponse {
+    private long orderId;
+    private Instant orderDate;
+    private String orderStatus;
+    private long amount;
+    private ProductDetails productDetails;
+    private PaymentDetails paymentDetails;
+}
