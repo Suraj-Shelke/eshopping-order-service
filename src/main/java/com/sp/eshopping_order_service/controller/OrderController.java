c4c77866d2fa1a0f9786cfe61f591dd830d2a2d8
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-22 23:03:23 +0530

    get order endpoint

--- src/main/java/com/sp/eshopping_order_service/controller/OrderController.java
+++ src/main/java/com/sp/eshopping_order_service/controller/OrderController.java
@@ -1,19 +1,37 @@
 package com.sp.eshopping_order_service.controller;
 
 import com.sp.eshopping_order_service.payload.request.OrderRequest;
+import com.sp.eshopping_order_service.payload.response.OrderResponse;
+import com.sp.eshopping_order_service.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+
+    private final OrderService orderService;
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @PostMapping("/placeorder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
+        long orderId = orderService.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderId,HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>(1l,HttpStatus.OK);
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
+
+        OrderResponse orderResponse
+                = orderService.getOrderDetails(orderId);
+
+        return new ResponseEntity<>(orderResponse,
+                HttpStatus.OK);
     }
 }
