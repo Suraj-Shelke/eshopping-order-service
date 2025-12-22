048365a3cbf0557c57908b42587b08cac1dd3bc1
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-22 23:08:23 +0530

    logs

--- src/main/java/com/sp/eshopping_order_service/controller/OrderController.java
+++ src/main/java/com/sp/eshopping_order_service/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.sp.eshopping_order_service.controller;
 import com.sp.eshopping_order_service.payload.request.OrderRequest;
 import com.sp.eshopping_order_service.payload.response.OrderResponse;
 import com.sp.eshopping_order_service.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
 
@@ -21,16 +23,19 @@ public class OrderController {
 
     @PostMapping("/placeorder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("OrderController | placeOrder is called");
+        log.info("OrderController | placeOrder | orderRequest: {}", orderRequest.toString());
         long orderId = orderService.placeOrder(orderRequest);
+        log.info("Order Id: {}", orderId);
         return new ResponseEntity<>(orderId,HttpStatus.OK);
     }
 
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
-
+        log.info("OrderController | getOrderDetails is called");
         OrderResponse orderResponse
                 = orderService.getOrderDetails(orderId);
-
+        log.info("OrderController | getOrderDetails | orderResponse : " + orderResponse.toString());
         return new ResponseEntity<>(orderResponse,
                 HttpStatus.OK);
     }
