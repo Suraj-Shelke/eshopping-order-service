9ff6e97a322cae2f483b86a0356fc4c39847b140
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-26 21:51:39 +0530

    fix feignclient

--- src/main/java/com/sp/eshopping_order_service/EshoppingOrderServiceApplication.java
+++ src/main/java/com/sp/eshopping_order_service/EshoppingOrderServiceApplication.java
@@ -2,12 +2,26 @@ package com.sp.eshopping_order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class EshoppingOrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EshoppingOrderServiceApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate
+				= new RestTemplate();
+		return restTemplate;
+	}
 }
